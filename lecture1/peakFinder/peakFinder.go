f55cb90706f0aa49eb8792b4e22587b66432ce79
Giorgos Alexakis <galexakhs97@gmail.com>
2022-06-22 12:04:46 +0300

    add startFromMiddleSolution

--- lecture1/peakFinder/peakFinder.go
+++ lecture1/peakFinder/peakFinder.go
@@ -30,13 +30,32 @@ func StraightForwardSolution(nums []int) int {
 func startFromMiddleSolution(nums []int) int {
 	index := 0
 	var b func(int, int)
+	if len(nums) == 1 {
+		return 0
+	}
 	b = func(left int, right int) {
 		mid := (right-left)/2 + left
-		if nums[mid] < nums[mid-1] {
-			right = mid - 1
-			b(left, right)
-		} else if nums[mid+1] > nums[mid] {
+		if mid == len(nums)-1 {
+			if nums[mid] >= nums[mid-1] {
+				index = mid
+			} else {
+				index = mid + 1
+			}
+			return
+		}
+		if mid == 0 {
+			if nums[mid] >= nums[mid+1] {
+				index = mid
+			} else {
+				index = mid + 1
+			}
+			return
+		}
+		if nums[mid] < nums[mid+1] {
 			left = mid + 1
+			b(left, right)
+		} else if nums[mid] < nums[mid-1] {
+			right = mid - 1
 			b(left, right)
 		} else {
 			index = mid
